8ef33e67dea157e3aea4cde8ccf6be7792b9390d
Mohd Muzakkir Saifi <87642050+muzakkirsaifi123@users.noreply.github.com>
2023-05-08 12:37:43 +0530

    Add files via upload

---
+++ app_service_plan/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "test" {
+  name     = "app-service-plan-rg-logicapp"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "test-appserviceplan"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "NaaS-app-service"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  app_service_plan_id = azurerm_app_service_plan.test.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
